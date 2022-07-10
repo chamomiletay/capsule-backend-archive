e74eecf6a4935636113621906a5b8a9ca8d79e24
chamomiletay <chamomile.tay@gmail.com>
2022-07-10 00:00:24 -0400

    Connected to database

--- capsule_django/capsule_django/settings.py
+++ capsule_django/capsule_django/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'capsule_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'capsule',
+        'USER': 'capsuleuser',
+        'PASSWORD': 'capsule',
+        'HOST': 'localhost',
     }
 }
 
