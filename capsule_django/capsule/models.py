d1be59a4e9b9d0ba261c473b4df936ddfe95cdc0
chamomiletay <chamomile.tay@gmail.com>
2022-07-13 23:58:20 -0400

    Created initial Article model

--- capsule_django/capsule/models.py
+++ capsule_django/capsule/models.py
@@ -1,3 +1,19 @@
+from email.mime import image
+from io import BufferedRandom
+from turtle import color
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    name = models.TextField()
+    color = models.TextField()
+    image_url = models.TextField()
+    article_type = models.TextField()
+    category = models.TextField()
+    brand = models.TextField()
+    quantity = models.TextField()
+
+    def __str__(self):
+        return self.name
